6c14f3061b54ab5206ab50eaf9fe62430b8d5cf9
jordon <jordon@tenet.com>
2024-02-12 18:03:59 -0500

    Implement selectColumns function to filter and return selected columns

--- src/util/selectColumns.ts
+++ src/util/selectColumns.ts
@@ -1,11 +1,31 @@
 import { TableRow, TableSchema } from "../types";
 
-export type Params = {
+type Params = {
   selectedColumns: string[];
   schema: TableSchema;
   row: TableRow;
 };
 
 export const selectColumns = (params: Params): TableRow => {
-  throw new Error("Not implemented");
+  const { schema, row } = params;
+  const selectedColumns = params.selectedColumns.map(trimColName);
+  if (selectedColumns.length === 0) {
+    throw new Error("selectedColumns must not be empty");
+  }
+  if (selectedColumns.includes("*")) {
+    return row;
+  }
+  const filteredRow: TableRow = {};
+  selectedColumns.forEach((col) => {
+    const value = row[col];
+    if (value === undefined) {
+      console.log({ row });
+      throw new Error(`Column ${col} does not exist in ${schema.tableName}`);
+    }
+    filteredRow[col] = value;
+  });
+  return filteredRow;
 };
+
+/** Remove any quotations from the column key */
+export const trimColName = (col: string) => col.replace(/\"|\'/gm, "").trim();
