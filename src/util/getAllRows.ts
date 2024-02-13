4fee3d30ab1a9eac028cbc0a99f00b1ec288fdad
jordon <jordon@tenet.com>
2024-02-12 20:56:14 -0500

    Refactor getAllRows function signature and add JSDoc comments

--- src/util/getAllRows.ts
+++ src/util/getAllRows.ts
@@ -5,8 +5,17 @@ type Params = {
   tableName: string;
 };
 
-export const getAllRows = (params: Params): ParsedTable | null => {
-  const { parsedData, tableName } = params;
+/**
+ * Retrieves all rows from the parsed data for a specific table.
+ * @param {Params} params - The parameters for retrieving the rows.
+ * @param {ParsedData} params.parsedData - The parsed data containing multiple tables.
+ * @param {string} params.tableName - The name of the table to retrieve rows from.
+ * @returns {ParsedTable | null} - The retrieved table or null if not found.
+ */
+export const getAllRows = ({
+  parsedData,
+  tableName,
+}: Params): ParsedTable | null => {
   const table = parsedData.find((t) => t.schema.tableName === tableName);
   return table || null;
 };
